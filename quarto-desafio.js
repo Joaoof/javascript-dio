b2dbf6d0c96b97c2000a473f035f70c1074440ec
joao <joaodeus400@gmail.com>
2023-09-29 16:11:33 -0300

    4 desafio

---
+++ quarto-desafio.js
@@ -0,0 +1,19 @@
+// formula do imc IMC = peso / (altura * altura)
+
+const peso = 70
+const altura = 1.75
+const IMC = peso / (altura * altura)
+
+if (IMC < 18.5) {
+    console.log("Abaixo do peso")
+} else if (IMC >= 18.5 && IMC < 25) {
+    console.log("Peso normal")
+} else if (IMC >= 25 && IMC < 30) {
+    console.log("Acima do Peso")
+} else if (IMC >= 30 && IMC < 40) {
+    console.log("Obeso")
+} else {
+    console.log("Você morreu")
+}
+
+console.log(`Seu peso é: ${IMC.toFixed(2)}`);
