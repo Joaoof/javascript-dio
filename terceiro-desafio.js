37309449dfba3bd0f097ae46adaa3fed4a8816e6
joao <joaodeus400@gmail.com>
2023-09-29 14:56:00 -0300

    desafio

--- terceiro-desafio.js
+++ terceiro-desafio.js
@@ -6,12 +6,13 @@
 
 const notas = [10, 40, 50]
 
-const media = (notas[0] + notas[1] + notas[2]) / 5
+const media = (notas[0] + notas[1] + notas[2]) / 3
 
 if (media < 5 ) {
     console.log("Você foi reprovado")
 } else if (media >= 5 && media <= 7) {
     console.log("Recuperação")
 } else {
-    console.log("Você foi aprovado")
+    console.log("Você foi aprovado hehe!")
 }
+
