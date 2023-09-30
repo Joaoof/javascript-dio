50e00ca383e5a8e463274fc8afe7051b872ab94a
Joaoof <joaodeus400@gmail.com>
2023-09-30 12:27:06 -0300

    quinto

--- quinto-desafio.js
+++ quinto-desafio.js
@@ -1,21 +1,36 @@
 // produtos 
-const produto1 = 200.00
-const produto2 = 400.00
-const produto3 = 700.00
+const produto1 = 100.00
+const produto2 = 100.00
+const produto3 = 100.00
 
 // descontos 
-const debito = 10 / 100
-const pix = 15 / 100
+const debito = false
+const pix = false
+
+const parcelar2x = false
+
+const juros = 0.10
+const numParcelas = 3
 
 // formula de calculo
 const soma = produto1 + produto2 + produto3
-const dezPorcento = debito * soma
-const quinzePorcento = pix * soma
-const result = soma - quinzePorcento
 
 // validação do desconto 
-if (soma >= 1000 ) {
-    console.log(`Você recebeu 10% de desconto ${dezPorcento} valor final, valor final ${result}`);
+if (debito) {
+    valorComDesconto = 0.10 * soma
+    console.log(valorComDesconto)
+    console.log(`valor com desconto débito: ${valorComDesconto}, valor final do produto ${soma - valorComDesconto}`)
+} else if (pix) {
+    let valorComDesconto = 0.15 * soma
+    console.log(`Você recebeu 15% de desconto ${valorComDesconto}, valor final ${soma  - valorComDesconto}`);
+} else if (parcelar2x) {
+    let dividir =  soma / 2
+    console.log(`Você pode dividir em 2 vezes, ${dividir}, valor final ${soma}`);
 } else {
-    console.log('Inválido');
+   if (numParcelas >= 3) {
+    let jurosA = soma * juros * numParcelas
+    let result = soma + jurosA
+
+    console.log(`Seu valor de ${numParcelas} na parcela resultou 10% de juros, ficando o valor: ${result}`);
+   }
 }
